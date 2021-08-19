8d3e00607f7c1435b50024ccbac73601a368f63f
nivcoo <nico64800@live.fr>
2021-08-20 00:54:09 +0200

    improv. remove useless code into enum file

--- src/main/java/fr/nivcoo/challenges/challenges/challenges/Types.java
+++ src/main/java/fr/nivcoo/challenges/challenges/challenges/Types.java
@@ -2,18 +2,6 @@ package fr.nivcoo.challenges.challenges.challenges;
 
 public enum Types {
 
-	BLOCK_BREAK("block_break"), BLOCK_PLACE("block_place"), ENTITY_DEATH("entity_death"), FISHING("fishing"),
-	ENCHANT_ALL("enchant_all");
-
-	private String challengeType;
-
-	private Types(String challengeType) {
-		this.challengeType = challengeType;
-	}
-
-	public String getChallengeType() {
-		return this.challengeType;
-	}
-
+	BLOCK_BREAK, BLOCK_PLACE, ENTITY_DEATH, FISHING, ENCHANT_ALL;
 
 }
